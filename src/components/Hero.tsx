ff7b639926970e94e6bf4e842989ffab5429c50f
Jin <breadofdestiny@gmail.com>
2026-02-09 12:40:14 +0700

    feat: refractor hero,add i18n

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,35 @@
 'use client'
 
 import React from 'react'
-import { HeroCarousel } from './HeroCarousel'
+import { HeroCarousel } from './carousel/HeroCarousel'
+import { Button } from './button'
 import heroBg3 from '../assets/banner-test3.jpg'
 import heroBg2 from '../assets/bannner-test2.jpg'
 import heroBg1 from '../assets/d048eb9439e681c322341cf84065711c767aa9b7.png'
 
+import { useTranslations } from 'next-intl'
+
 export const Hero: React.FC = () => {
+  const t = useTranslations('Home')
   const carouselImages = [heroBg3, heroBg2, heroBg1]
 
   return (
-    <section className="relative h-screen w-full overflow-hidden bg-neutral-900 text-white">
+    <section className="relative md:h-screen w-full h-fit overflow-hidden bg-neutral-900 text-white">
       <HeroCarousel images={carouselImages} />
 
-      <div className="relative z-10 flex h-full w-full max-w-[468px] flex-col justify-center px-24 py-24 pointer-events-none bg-black/40 gap-9">
+      <div className="relative z-10 flex md:h-screen h-fit md:w-full md:max-w-[468px] flex-col justify-center px-6 py-12 lg:px-24 lg:py-24 pointer-events-none bg-black/40 gap-9">
         <div className="max-w-xl flex flex-col pointer-events-auto gap-6">
-          <h1 className="h1">
-            <span className="block text-white">PUSH</span>
+          <h1 className="h1 leading-[120%] tracking-wider md:leading-[100%] md:tracking-wider">
+            <span className="block text-white">{t('Hero.title_part1')}</span>
             <span className="bg-linear-to-r from-blue-600 via-purple-500 to-pink-500 bg-clip-text text-transparent">
-              BEYOND
+              {t('Hero.title_part2')}
             </span>
-            <span className="block text-white">LIMITS</span>
+            <span className="block text-white">{t('Hero.title_part3')}</span>
           </h1>
 
-          <p className="body-sm">
-            Your Partner for World-Class Sports Facility Development. Lorem ipsum dolor sit amet
-            consectetur.
-          </p>
+          <p className="body-sm">{t('Hero.description')}</p>
         </div>
-        <button className="btn btn-gradient-solid-border btn-sm-typo text-white  w-[154px]">
-          <span>Contact Us</span>
-        </button>
+        <Button className="w-[154px] md:w-[190px] text-white">{t('Hero.contact_us')}</Button>
       </div>
     </section>
   )
