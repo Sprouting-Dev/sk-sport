60349b906ddddc91baeb6974b94841ad48a4ba94
Kiattikoon-exe <kknippy@gmail.com>
2026-02-10 11:52:30 +0700

    feat: add footer component, mobile layout

---
+++ src/app/(frontend)/footer/page.tsx
@@ -0,0 +1,9 @@
+import { Footer } from '@/components/footer/footer'
+
+export default function FooterPage() {
+  return (
+    <div className="min-h-screen flex items-end justify-center ">
+      <Footer />
+    </div>
+  )
+}
--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { rajdhani, notoSansThai } from '@/lib/fonts'
 import { Navbar } from '@/components/layout/'
+import { rajdhani, notoSansThai, prompt } from '@/lib/fonts'
 import '@/style/typography.css'
 import './styles.css'
+import { NextIntlClientProvider } from 'next-intl'
+import { getLocale, getMessages } from 'next-intl/server'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
@@ -11,12 +14,19 @@ export const metadata = {
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
+  const locale = await getLocale()
+  const messages = await getMessages()
 
   return (
     <html lang="en" data-theme="sksport">
       <body className={`${rajdhani.variable} ${notoSansThai.variable}`}>
         <Navbar />
         <main className="pt-20">{children}</main>
+    <html lang={locale} data-theme="sksport">
+      <body className={`${rajdhani.variable} ${notoSansThai.variable} ${prompt.variable}`}>
+        <NextIntlClientProvider messages={messages}>
+          <main>{children}</main>
+        </NextIntlClientProvider>
       </body>
     </html>
   )
