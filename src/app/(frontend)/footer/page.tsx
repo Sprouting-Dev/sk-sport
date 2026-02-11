c73205d75ef6d5042f658f322468eae0a1334329
Kiattikoon-exe <655021000022@mail.rmutk.ac.th>
2026-02-11 14:38:04 +0700

    fix: seperate icon to component, add footer to layout folder

--- src/app/(frontend)/footer/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Footer } from '@/components/footer/footer'
-
-export default function FooterPage() {
-  return (
-    <div className="min-h-screen flex items-end justify-center ">
-      <Footer />
-    </div>
-  )
-}
